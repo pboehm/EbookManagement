d1ca9e199cdad4e9fe5b6a792fe1f98220931a53
Philipp Böhm <philipp-boehm@live.de>
2011-12-16 22:21:06 +0100

    Konfiguration verallgemeinert, sodass das Setzen von Debug=False reicht um es einfach auf dem Server zu deployen

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from ebooks import views
-from settings import *
+import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,9 +12,10 @@ urlpatterns = patterns('',
     url(r'show/(?P<type>\w+)/(?P<dataid>\d+)/', views.show_data ),
     url(r'manage/ebooks/', views.manage_ebooks ),
 
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve'),
-
     # admin
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+
