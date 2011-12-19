d158c7abf1eb884cc1b1368f6126bd1f35485128
Philipp Böhm <philipp-boehm@live.de>
2011-12-19 22:14:13 +0100

    zugehörige Scripte integriert

---
+++ ebooks/event_processors.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import pyinotify
+
+class EbookChangeProcessor(pyinotify.ProcessEvent):
+    
+    def process_IN_CREATE(self, event):
+        print "IN CREATE: %s" % event.pathname
+
+    def process_IN_DELETE(self, event):
+        print "IN DELETE: %s" % event.pathname
